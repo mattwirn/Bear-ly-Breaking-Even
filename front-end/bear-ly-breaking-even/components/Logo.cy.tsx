41877698b731fb01155a88a82ae2132cd1af15ca
Yehuda <yehuda.c003@gmail.com>
2023-02-20 15:57:13 -0500

    created tests for components using cypress

--- front-end/bear-ly-breaking-even/components/Logo.cy.tsx
+++ front-end/bear-ly-breaking-even/components/Logo.cy.tsx
@@ -6,8 +6,8 @@ describe('<Logo />', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<Logo w={40} />)
   })
-  it('should show the icon', () => {
-    cy.mount(<Logo w={40} />)
-    cy.get('[data-cy=logo]').should('be.visible')
+  it('should render the icon', () => {
+    cy.mount(<Logo w={100} />)
+    cy.get('img').should('be.visible')
   })
 })
--- front-end/bear-ly-breaking-even/components/PageHeader.cy.tsx
+++ front-end/bear-ly-breaking-even/components/PageHeader.cy.tsx
@@ -7,8 +7,10 @@ describe('<PageHeader />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<PageHeader />)
-    cy.mount(<Logo w={40}/>)
-    const pathname = '../public/logo.png'
+  })
+  it("dispays the header component", () => {
+    cy.mount(<PageHeader />)
+    const pathname = '../pages/index.tsx'
     const push = cy.stub()
     cy.stub(NextRouter, 'useRouter').returns({ pathname, push })
   })
