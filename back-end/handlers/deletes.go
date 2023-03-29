5f8f7f6737195433cee9e2d7a3a4d2c5f20088b6
mattwirn <m.wirnowski@ufl.edu>
2023-03-28 23:19:53 -0400

    Added delete and update functionality to expenses

---
+++ back-end/handlers/deletes.go
@@ -0,0 +1,78 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/mattwirn/Bear-ly-Breaking-Even/back-end/initializers"
+	"github.com/mattwirn/Bear-ly-Breaking-Even/back-end/models"
+)
+
+func DeleteExpense(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Username    string
+		ExpenseType string
+		ExpenseName string
+		Amount      uint
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	switch body.ExpenseType {
+	case "HomeUtils":
+		var user models.Home_Uts
+		result := initializers.DB.Where("username = ?", body.Username).Where("expensename = ?", body.ExpenseName).Where("amount = ?", body.Amount).Delete(&user)
+
+		if result.Error != nil {
+			http.Error(w, "Failed to delete expense", http.StatusInternalServerError)
+			return
+		}
+
+	case "Transportation":
+		var user models.Trans
+		result := initializers.DB.Where("username = ?", body.Username).Where("expensename = ?", body.ExpenseName).Where("amount = ?", body.Amount).Delete(&user)
+
+		if result.Error != nil {
+			http.Error(w, "Failed to delete expense", http.StatusInternalServerError)
+			return
+		}
+
+	case "Food":
+		var user models.Food
+		result := initializers.DB.Where("username = ?", body.Username).Where("expensename = ?", body.ExpenseName).Where("amount = ?", body.Amount).Delete(&user)
+
+		if result.Error != nil {
+			http.Error(w, "Failed to delete expense", http.StatusInternalServerError)
+			return
+		}
+
+	case "Education":
+		var user models.Edu
+		result := initializers.DB.Where("username = ?", body.Username).Where("expensename = ?", body.ExpenseName).Where("amount = ?", body.Amount).Delete(&user)
+
+		if result.Error != nil {
+			http.Error(w, "Failed to delete expense", http.StatusInternalServerError)
+			return
+		}
+
+	case "Health":
+		var user models.Health
+		result := initializers.DB.Where("username = ?", body.Username).Where("expensename = ?", body.ExpenseName).Where("amount = ?", body.Amount).Delete(&user)
+
+		if result.Error != nil {
+			http.Error(w, "Failed to delete expense", http.StatusInternalServerError)
+			return
+		}
+
+	default:
+		http.Error(w, "Failed to delete expense, expense type not found", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Expense Deleted\n"))
+}
---
+++ back-end/handlers/puts.go
@@ -0,0 +1,100 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/mattwirn/Bear-ly-Breaking-Even/back-end/initializers"
+	"github.com/mattwirn/Bear-ly-Breaking-Even/back-end/models"
+)
+
+func UpdateExpense(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Username       string
+		ExpenseType    string
+		OldExpenseName string
+		OldAmount      uint
+		NewExpenseName string
+		NewAmount      uint
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	switch body.ExpenseType {
+	case "HomeUtils":
+		var user models.Home_Uts
+
+		result := initializers.DB.Where("username = ?", body.Username).Where("expensename = ?", body.OldExpenseName).Where("amount = ?", body.OldAmount).First(&user)
+		if result.Error != nil {
+			http.Error(w, "Failed to find expense", http.StatusInternalServerError)
+			return
+		}
+
+		user.ExpenseName = body.NewExpenseName
+		user.Amount = body.NewAmount
+		initializers.DB.Save(&user)
+
+	case "Transportation":
+		var user models.Trans
+
+		result := initializers.DB.Where("username = ?", body.Username).Where("expensename = ?", body.OldExpenseName).Where("amount = ?", body.OldAmount).First(&user)
+		if result.Error != nil {
+			http.Error(w, "Failed to find expense", http.StatusInternalServerError)
+			return
+		}
+
+		user.ExpenseName = body.NewExpenseName
+		user.Amount = body.NewAmount
+		initializers.DB.Save(&user)
+
+	case "Food":
+		var user models.Food
+
+		result := initializers.DB.Where("username = ?", body.Username).Where("expensename = ?", body.OldExpenseName).Where("amount = ?", body.OldAmount).First(&user)
+		if result.Error != nil {
+			http.Error(w, "Failed to find expense", http.StatusInternalServerError)
+			return
+		}
+
+		user.ExpenseName = body.NewExpenseName
+		user.Amount = body.NewAmount
+		initializers.DB.Save(&user)
+
+	case "Education":
+		var user models.Edu
+
+		result := initializers.DB.Where("username = ?", body.Username).Where("expensename = ?", body.OldExpenseName).Where("amount = ?", body.OldAmount).First(&user)
+		if result.Error != nil {
+			http.Error(w, "Failed to find expense", http.StatusInternalServerError)
+			return
+		}
+
+		user.ExpenseName = body.NewExpenseName
+		user.Amount = body.NewAmount
+		initializers.DB.Save(&user)
+
+	case "Health":
+		var user models.Health
+
+		result := initializers.DB.Where("username = ?", body.Username).Where("expensename = ?", body.OldExpenseName).Where("amount = ?", body.OldAmount).First(&user)
+		if result.Error != nil {
+			http.Error(w, "Failed to find expense", http.StatusInternalServerError)
+			return
+		}
+
+		user.ExpenseName = body.NewExpenseName
+		user.Amount = body.NewAmount
+		initializers.DB.Save(&user)
+
+	default:
+		http.Error(w, "Failed to update expense, expense type not found", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Expense Updated\n"))
+}
