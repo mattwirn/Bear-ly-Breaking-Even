2cb2586f355f0d5b4720cd503106f67182968a8d
Yehuda <yehuda.c003@gmail.com>
2023-04-18 00:12:09 -0400

    fixed up dashboard a bit

---
+++ front-end/bear-ly-breaking-even/components/ExpenseTable.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { TrashIcon, PencilIcon } from '@heroicons/react/solid';
+
+export default function Table() {
+  const [rows, setRows] = useState([
+    ['Row 1, Column 1', 'Row 1, Column 2']
+
+  ]);
+
+  const addRow = () => {
+    const newRow = ['', ''];
+    setRows([...rows, newRow]);
+  };
+
+  const deleteRow = (index) => {
+    const newRows = [...rows];
+    newRows.splice(index, 1);
+    setRows(newRows);
+  };
+
+  const editRow = (index, colIndex, value) => {
+    const newRows = [...rows];
+    newRows[index][colIndex] = value;
+    setRows(newRows);
+  };
+
+  function showInputFields() {
+
+  }
+
+  return (
+    <div className='mx-10'>
+      <table className='border border-black-700'>
+        <tbody>
+          {rows.map((row, index) => (
+            <tr key={index}>
+              <td className='border border-slate-700'>
+                <div className='flex justify-between'>
+                  <input
+                    className='mx-3 my-2 border border-slate-700'
+                    type='text'
+                    value={row[0]}
+                    onChange={(e) => editRow(index, 0, e.target.value)}
+                  />
+                  <input
+                    className='my-2 border border-slate-700'
+                    type='text'
+                    value={row[1]}
+                    onChange={(e) => editRow(index, 1, e.target.value)}
+                  />
+                  <div className='flex justify-end'>
+                  <button onClick={showInputFields} className='my-2'>
+                      <PencilIcon className='h-4 w-4 text-blue-500' />
+                    </button>
+                    <button
+                      className=' my-2'
+                      onClick={() => deleteRow(index)}
+                    >
+                      <TrashIcon className='h-4 w-4 text-red-500' />
+                    </button>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          ))}
+          <tr>
+            <td className='border border-black-700'>
+              <button className='mx-1 my-2 hover:underline' onClick={addRow}>
+                Add Row
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
